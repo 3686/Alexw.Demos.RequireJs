3da33e68dcf071736de70fda54fd11a66f595d35
Alexander Williamson <Alexander Williamson>
2016-10-04 23:54:11 +0100

    wip jasmine

--- Alexw.Demos.RequireJs.Website/scripts/application.js
+++ Alexw.Demos.RequireJs.Website/scripts/application.js
@@ -1,5 +1,8 @@
 define(['consoleWriter', 'htmlWriter'], function(consoleWriter, htmlWriter) {
 
+    // this is a module
+    // this needs to be a file, and use the define method
+
     var Application = function() {
         this.name = "Application";
     };
